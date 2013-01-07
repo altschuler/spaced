ffc575da32c7616514d0d41a6c1fea5cfa7a961a
Markus Færevaag <mafaer@gmail.com>
2013-01-07 11:34:22 +0100

    Hej

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hej");
-		//test
-		System.out.println("Patrick er en tard");
-		// commenting
+		System.out.println("Hello, World!");
 	}
 }
