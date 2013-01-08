64c84196a8705dac3c563c91106d8a836c55373f
Simon Altschuler <simon@altschuler.dk>
2013-01-08 13:45:19 +0100

    basic player movement

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -7,7 +7,6 @@ import javax.swing.Timer;
 import model.GameModel;
 import model.GameState;
 import model.GameStateFactory;
-import model.elements.Invader;
 import utils.Input;
 import view.MainView;
 
@@ -21,18 +20,23 @@ public class GameController extends AbstractController {
 	public GameController(MainView gw, GameModel gm) {
 		super(gw, gm);
 
-		gm.setActiveGameState(GameStateFactory.createLevelOne()); // TODO this is BAD
-
+		GameState gameState = GameStateFactory.createLevelOne();
+		gameState.setLastUpdateTime(System.currentTimeMillis());
+		gm.setActiveGameState(gameState);
 	}
 	
 	private Timer getTimer() {
 		if (this.timer == null) {
-			this.timer = new Timer(200, new CommandListener(CommandFactory.createUpdateGameStateCommand(gameModel.getActiveGameState())));
+			this.timer = new Timer(100, new CommandListener(CommandFactory.createUpdateGameStateCommand(gameModel.getActiveGameState())));
 		}
 		
 		return timer;
 	}
 
+	/**
+	 * Sets the enabled state of the game loop, effectively starting and stopping a game session
+	 * @param enabled Whether the loop will run or not
+	 */
 	public void setGameLoopEnabled(boolean enabled) {
 		if (enabled) {
 			this.getTimer().start();
@@ -47,21 +51,30 @@ public class GameController extends AbstractController {
 	 *            in time.
 	 */
 	public void updateGameState(GameState gameState) {
-		// TODO implement
+
 		long currentTime = System.currentTimeMillis();
 		long timeDelta = currentTime - gameState.getLastUpdateTime();
 		
-		for (Invader inv : gameState.getInvaders()) {
-			//inv.getPosition().x
-		}
-		
+		// Update Player
 		if (Input.getInstance().isKeyDown(KeyEvent.VK_LEFT)) {
-			System.out.println(String.format("Go left %d time", timeDelta));
-			
+			gameState.getPlayer().getPosition().x -= this.distance(timeDelta, 5);
+		}
+		else if (Input.getInstance().isKeyDown(KeyEvent.VK_RIGHT)) {
+			gameState.getPlayer().getPosition().x += this.distance(timeDelta, 5);
 		}
 		
-		System.out.println(timeDelta);
+		System.out.println(String.format("Player is at %s", gameState.getPlayer().getPosition().toString()));
+		
+		// Update time stamp
 		gameState.setLastUpdateTime(currentTime);
 	}
 
+	/**
+	 * @param timeDelta Time passed since last frame
+	 * @param speed How fast is the object moving
+	 * @return A distance dependant on time, thereby avoiding glitches that make movement unstable
+	 */
+	private long distance(long timeDelta, int speed) {
+		return timeDelta/100 * speed;
+	}
 }
