463f09282ad8e4121507b761abbf759cb6881e35
patrickgadd <patrick_gadd@hotmail.com>
2013-01-08 11:12:00 +0100

    tilfoejet KeyListener til MainView (da det er en frame og KeyListeners kun kan addes til disse)

--- src/views/MainView.java
+++ src/views/MainView.java
@@ -4,6 +4,8 @@ import java.awt.Rectangle;
 
 import javax.swing.JFrame;
 
+import controllers.GameKeyboardListener;
+
 /**
  * The main window inside which states are rendered
  */
@@ -15,5 +17,6 @@ public class MainView extends JFrame {
 		this.setBounds(new Rectangle(0, 0, 300, 300));
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.addKeyListener(new GameKeyboardListener());
 	}
 }
