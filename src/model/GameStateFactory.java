9b9252b01bb77300679488817a76362de5ed10ac
Markus Færevaag <mafaer@gmail.com>
2013-01-10 10:55:45 +0100

    nvm

--- src/model/GameStateFactory.java
+++ src/model/GameStateFactory.java
@@ -8,11 +8,6 @@ import java.util.ArrayList;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import model.elements.Bunker;
-import model.elements.Invader;
-import model.elements.InvaderType;
-import model.elements.Player;
-import model.elements.PlayerIndex;
 import org.xml.sax.SAXException;
 import utils.SaxGameStateHandler;
 
@@ -36,8 +31,6 @@ public class GameStateFactory {
             saxParser.parse(xmlInput, handler);
             
             this.levels = handler.getLevels();
-            
-            levels.get(0).printInfo();
     
         } catch (FileNotFoundException e) {
             System.out.println("ERROR: Cannot find file: "+XML_FILE);
@@ -58,36 +51,36 @@ public class GameStateFactory {
 		return levels.get(id);
 	}
 
-    static public GameState createLevelOne() {
-            GameState gs = new GameState(0);
-
-            // Bunkers
-            Bunker b1 = new Bunker();
-            b1.getPosition().x = 50;
-            b1.getPosition().y = 450;
-            gs.getBunkers().add(b1);
-
-            // Player
-            gs.setPlayer(PlayerIndex.One, new Player(3));
-            gs.getPlayer(PlayerIndex.One).getPosition().x = 250 - 24;
-            gs.getPlayer(PlayerIndex.One).getPosition().y = 600;
-
-            // Invaders
-            int invaderCounter = 0, columnsOfInvaders = 7, rowsOfInvaders = 3, widthBetweenInvaders = 55, heightBetweenInvaders = 50;
-            int xInvaderStart = 50, yInvaderStart = 50;
-            int invaderHealth = 2;
-            for (int i = 0; i < columnsOfInvaders; i++) {
-            	for (int j = 0; j < rowsOfInvaders; j++) {
-            		gs.getInvaders().add(new Invader(InvaderType.A, invaderHealth,"view/sprites/invader2.png"));
-                	gs.getInvaders().get(invaderCounter).getPosition().x = i*widthBetweenInvaders+xInvaderStart;
-                	gs.getInvaders().get(invaderCounter).getPosition().y = j*heightBetweenInvaders+yInvaderStart;
-                	invaderCounter++;
-				}
-            	
-			}
-            
-
-            return gs;
-    }
+//    static public GameState createLevelOne() {
+//            GameState gs = new GameState(0);
+//
+//            // Bunkers
+//            Bunker b1 = new Bunker();
+//            b1.getPosition().x = 50;
+//            b1.getPosition().y = 450;
+//            gs.getBunkers().add(b1);
+//
+//            // Player
+//            gs.setPlayer(PlayerIndex.One, new Player(3));
+//            gs.getPlayer(PlayerIndex.One).getPosition().x = 250 - 24;
+//            gs.getPlayer(PlayerIndex.One).getPosition().y = 600;
+//
+//            // Invaders
+//            int invaderCounter = 0, columnsOfInvaders = 7, rowsOfInvaders = 3, widthBetweenInvaders = 55, heightBetweenInvaders = 50;
+//            int xInvaderStart = 50, yInvaderStart = 50;
+//            int invaderHealth = 2;
+//            for (int i = 0; i < columnsOfInvaders; i++) {
+//            	for (int j = 0; j < rowsOfInvaders; j++) {
+//            		gs.getInvaders().add(new Invader(InvaderType.A, invaderHealth,"view/sprites/invader2.png"));
+//                	gs.getInvaders().get(invaderCounter).getPosition().x = i*widthBetweenInvaders+xInvaderStart;
+//                	gs.getInvaders().get(invaderCounter).getPosition().y = j*heightBetweenInvaders+yInvaderStart;
+//                	invaderCounter++;
+//				}
+//            	
+//			}
+//            
+//
+//            return gs;
+//    }
 
 }
